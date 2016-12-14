c74f27a2ae01691df34bb0aee55af21b24c7dd7b
Emile Keith <emilekeith3.0@gmail.com>
2016-12-14 18:53:26 -0500

    Update updateProject.sh

--- _vagrant/updateProject.sh
+++ _vagrant/updateProject.sh
@@ -3,12 +3,5 @@
 # Use single quotes instead of double quotes to make it work with special-character passwords
 PROJECTFOLDER='QMVC'
 
-
-# git pull QMVC
-if [ -d "/var/www/html/${PROJECTFOLDER}" ] 
-then
-    (exec "./bootstrap.sh") 
-else
-    cd "/var/www/html/${PROJECTFOLDER}"
-    sudo git pull
-fi
+cd "/var/www/html/${PROJECTFOLDER}"
+sudo git pull
