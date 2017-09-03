0edaaeb156cb0d18844a299b895157d600a25e02
Emile Keith <emilekeith3.0@gmail.com>
2017-09-03 08:45:59 -0500

    Expanded and corrected middleware.php

--- Base/Routing/Middleware.php
+++ Base/Routing/Middleware.php
@@ -9,15 +9,16 @@ class Middleware
     private $next = null;
     private $handler = null;
 
-    function __construct($next = null)
+    function __construct($next = null, $handler = null)
     {
-        if($next != null)
-            $this->setHandler($next);
+        $this->setNext($next);
+        if($handler != null)
+            $this->setHandler($handler);
         else
             $this->setHandler(function($next, Request $request){});
     }
 
-    public function setHandler($next)
+    public function setNext($next)
     {
         if(!Helpers::isValidRouteHandler($next) && !is_a($next, static::class))
         {   
@@ -27,7 +28,22 @@ class Middleware
                 Constants::HANDLER_METHOD_SIG . "method ";
             throw new InvalidArgumentException($exceptionMessage);
         }
-        
+        $this->next = $nextS;
+    }
+
+    public function getNext()
+    {
+        return $this->next;
+    }
+
+    public function setHandler($handler)
+    {
+        throw new Exception("NOT YET IMPLEMENTED!");
+    }
+
+    public function getHandler()
+    {
+        return $this->handler;
     }
 
     public function invoke(Request $request)
