a2352654e2e1c594fe2b7a39cba72f4b3d6531c7
Emile Keith <emilekeith3.0@gmail.com>
2017-09-02 22:50:19 -0500

    Added test for get route callback

--- Base/Tests/Routing/RouteTest.php
+++ Base/Tests/Routing/RouteTest.php
@@ -12,6 +12,16 @@ class RouteTest extends TestCase
         $route = new Route($URI, [], function(){ return 'Hello World'; });
         $this->assertEquals($route->getURI(), Sanitizers::cleanURI($URI));
     }
+
+    public function testGetHandler()
+    {
+        $outputStr = 'Hello World';
+        $this->expectOutputString($outputStr);
+        $callback = function() use ($outputStr) { echo $outputStr; };
+        $route = new Route('/', [], $callback);
+        $handler = $route->getHandler();
+        $handler();
+    }
 }
 
 ?>
