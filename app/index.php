c1d79af537452a63cdf31cccc76a28204e8fbff0
Emile Keith <emilekeith3.0@gmail.com>
2017-09-17 16:49:50 -0500

    removed display logging

--- app/index.php
+++ app/index.php
@@ -2,10 +2,6 @@
 
 namespace App;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once('./config/constants.php');
 require_once('./config/security.php');
 require_once('./config/routes.php');
